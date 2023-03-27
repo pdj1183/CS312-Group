cea3acfafd429c180ec13948518c4f48901ff549
Phillip Johnson <pdj1183@gmail.com>
2023-03-27 13:15:08 -0600

    Working template navbar with active site

--- local_html/m1/index.php
+++ local_html/m1/index.php
@@ -1,30 +1,4 @@
 <?php
-/**
- * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
- *
- * @package    Fuel
- * @version    1.8.2
- * @author     Fuel Development Team
- * @license    MIT License
- * @copyright  2010 - 2019 Fuel Development Team
- * @link       https://fuelphp.com
- */
-
-/**
- * -----------------------------------------------------------------------------
- *  Configure PHP Settings
- * -----------------------------------------------------------------------------
- */
-
-/**
- * -----------------------------------------------------------------------------
- *  Show error reporting
- * -----------------------------------------------------------------------------
- *
- *  Set error reporting and display errors settings.
- *  You will want to change these when in production.
- *
- */
 
 error_reporting(-1);
 
--- views/m1template.php
+++ views/m1template.php
@@ -1,8 +1,21 @@
 <head>
     <meta charset="utf-8" />
-    <meta name="author" content="Phillip" />
-    <meta name="description" content="Fuel Template" />
 </head>
+
+<nav>
+    <h1 class="navItem">The Color Company</h1>
+    <ul class="navbar">
+        <li class="navItem <?php if($current_page == 'home') echo 'active'; ?>">
+            <a href="./index">Home</a>
+        </li>
+        <li class="navItem <?php if($current_page == 'color') echo 'active'; ?>">
+            <a href="./color">Color Generator</a>
+        </li>
+        <li class="navItem <?php if($current_page == 'about') echo 'active'; ?>">
+            <a href="./about">About</a>
+        </li>
+    </ul>
+</nav>
 
 <body>
     <main>
