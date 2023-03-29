2d7013cc91f20e2e6b296c1287da0975b5c189e7
Jalynn Nicoly <97772154+Jalynnn@users.noreply.github.com>
2023-03-28 22:49:36 -0600

    Update color.php

---
+++ views/m1/color.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Color Generator</title>
+</head>
+<body>
+    <label for = "rows" > Number of Rows/Columns: </label>
+    <input type = "number" id = "rows" name = "rows" min = "1" max = "26">
+    <label for = "colors" > Number of Colors: </label>
+    <input type = "number" id = "colors" name = "colors" min = "1" max = "10">
+    <input type = "submit" value = "Submit" >
+</body>
+</html>
+
+<!--Submit needs a follow up - not sure what this looks like yet
+    User input needs to be validated-->
