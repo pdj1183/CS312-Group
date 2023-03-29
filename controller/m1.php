50253995ec0be4aa26a3f40fd9a93e23ec28c269
Jalynn Nicoly <97772154+Jalynnn@users.noreply.github.com>
2023-03-29 13:09:12 -0600

    Update m1.php

--- controller/m1.php
+++ controller/m1.php
@@ -18,4 +18,29 @@ class Controller_m1 extends Controller_Template
 		$this->template->current_page = "about";
 		$this->template->content = View::forge('m1/about');
 	}
+	
+	public function ValidateInputParams($rows, $colors) {
+		// This is in regard to paragraph two but may not be neccessary
+		// due to including 'required' in the input type: 
+
+		// if(rows < 1 || rows > 26) {
+		// 	Print error is a work in progress
+		// 	try {
+		// 		$this->response->("Row is not valid", 400);
+		// 	} catch() {}
+		// }
+		// if(colors < 1 || colors > 10) {
+		// }
+
+
+
+
+		// This is in regard to generating two tables - def uses a for loop
+		// Currently doesn't work
+
+		// echo '<table>';
+		// for ($i = 1; $i <= 10; $i++)
+		// echo '<tr><td>' . $i*$i . "</td></tr>";
+		// echo '</table>';
+	}
 }
