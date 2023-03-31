3104a3b3e8f84da3481d92606aa674e8ead55ea3
Phillip Johnson <pdj1183@gmail.com>
2023-03-31 11:17:48 -0600

    Cool looking homepage

--- views/m1/index.php
+++ views/m1/index.php
@@ -8,6 +8,14 @@
     <meta name="description" content="Group milestone site for cs312">
 </head>
 
-<p>Home</p>
+<section class="banner">
+    <div class="content">
+        <h1 class="color">We Like Color!</h1>
+        <p class="company">
+            Here at The Color Company we aim to provide you, the user, with some really fun colors. We think.
+            Probably. Anyways here is our site and it should make some colors for you if we did some stuff right!
+        </p>
+    </div>
+</section>
 
 </html>
