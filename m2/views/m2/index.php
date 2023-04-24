079eab81243f150f4be26fd95b1ad2206fb098db
Phillip Johnson <pdj1183@gmail.com>
2023-04-24 15:26:28 -0600

    M1 done except for print view
    renamed to m2 https://www.cs.colostate.edu:4444/~pdj1183/m2/index/m2/index

---
+++ m2/views/m2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title> Milestone 2 </title>
+    <meta name="author" content="Group 7">
+    <meta name="description" content="Group milestone site for cs312">
+</head>
+
+<section class="banner">
+    <div class="content">
+        <h1 class="color">We Like Color!</h1>
+        <p class="company">
+            Here at The Color Company we aim to provide you, the user, with some really fun colors. We think.
+            Probably. Anyways here is our site and it should make some colors for you if we did some stuff right!
+        </p>
+    </div>
+</section>
+
+</html>
---
+++ m2/views/m2template.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="utf-8" />
+    <?php echo Asset::css($css) ?>
+</head>
+
+<div class="nav">
+    <nav>
+
+        <ul class="navbar<?php if($current_page == 'home') echo 'active'; ?>">
+            <li class="navItem">
+                <h1>The Color Company</h1>
+            </li>
+            <li class="navItem <?php if($current_page == 'home') echo 'active'; ?>">
+                <a href="./index">Home</a>
+            </li>
+            <li class="navItem <?php if($current_page == 'color') echo 'active'; ?>">
+                <a href="./colorForm">Color Generator</a>
+            </li>
+            <li class="navItem <?php if($current_page == 'about') echo 'active'; ?>">
+                <a href="./about">About</a>
+            </li>
+        </ul>
+        <div class="navImg">
+            <?php echo Asset::img("312logo.svg") ?>
+        </div>
+    </nav>
+</div>
+
+<div class="main">
+
+    <?php echo $content?>
+</div>
