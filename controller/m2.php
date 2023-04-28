65f98dca8e0fee810f409c0d35e5137b3964ff44
Phillip Johnson <pdj1183@gmail.com>
2023-04-28 13:54:30 -0600

    Print view
    Moved m2 into main and m1 into a folder

---
+++ controller/m2.php
@@ -0,0 +1,51 @@
+<?php
+
+
+use Fuel\Core\View;
+
+
+
+class Controller_m2 extends Controller_Template
+{
+
+	public $template = 'm2template';
+	
+	public function action_index() {
+		$this->template->current_page = "home";
+		$this->template->css = "main.css";
+		$this->template->content = View::forge('m2/index');
+	}
+
+	public function action_colorForm() {
+		$this->template->current_page = "color";
+		$this->template->css = "main.css";
+		$this->template->content = View::forge('m2/colorForm');
+	}
+
+	public function post_color() {
+		$row = Input::post('rows');
+		$colors = Input::post('colors');
+
+		
+
+		
+		if ($row > 0 && $row <= 26 && $colors > 0 && $colors <= 10){
+			$this->template->current_page = "color";
+			$this->template->css = "main.css";
+			$this->template->content = View::forge('m2/colorTable');
+			
+		} 
+		else {
+			$this->template->current_page = "color";
+			$this->template->css = "main.css";
+			$this->template->content = View::forge('m2/colorForm');
+		}
+	}
+	
+
+	public function action_about() {
+		$this->template->current_page = "about";
+		$this->template->css = "main.css";
+		$this->template->content = View::forge('m2/about');
+	}
+}
