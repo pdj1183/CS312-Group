7c2fbed9f63d54a0ebb76ef9fb87465ea1ce7171
AaronMarti34 <70295666+AaronMarti34@users.noreply.github.com>
2023-05-03 16:23:28 -0600

    Update colorTable.php

--- views/m2/colorTable.php
+++ views/m2/colorTable.php
@@ -32,13 +32,15 @@ function checkDropdowns(select) {
             $default_colors = array("black", "blue", "brown", "green", "grey", "orange", "purple", "red", "teal", "yellow");
             
             for ($i = 0; $i < $colors; $i++) {
-                echo '<tr>';
+                $row_id = 'color-row-' . ($i+1);
+                echo '<tr id="' . $row_id . '">';
                 echo  '<td class="color-sel" style="width: 20%;height: 25px">';
                 echo '<div class="color-list">';
-                echo '<button class="radio" id="radio" <h1> </h1></button>';   
+                echo '<input type="radio" name="color" class="radio" value="'.$default_colors[$i].'"></input>';   
                 echo '<select onchange="checkDropdowns(this)">';
                 echo '<option value="color" selected diabled hidden>Select a Color</option>';
                 
+
                 foreach($default_colors as $color) {
                     if ($color == $default_colors[$i]) {
                         echo "<option value='$color' selected>$color</option>";
@@ -55,6 +57,16 @@ function checkDropdowns(select) {
         }
     ?>
 </table>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    const colorRadioButtons = document.querySelectorAll('.color-sel input[type="radio"]');
+    let selectedColor = null;
+    colorRadioButtons.forEach(radioButton => {
+        radioButton.addEventListener('click', () => {
+            selectedColor = radioButton.value;
+        });
+    });
+    </script>
 
 <div class="fail_color hidden">
     <h1> Oh No! </h1>
@@ -82,15 +94,33 @@ function checkDropdowns(select) {
                         echo "<td>",$i+1,"</td>";
                     }
                     else{
-                    echo "<td></td>";
+                    echo "<td class='table-cell'></td>";
                     }
                 }
             }
             }
         }
+        
         ?>
 </table>
-
+<script>
+        const tableCells = document.querySelectorAll('.table-cell');
+        tableCells.forEach(tableCell => {
+            tableCell.addEventListener('click', () => {
+                if (!tableCell.parentNode.getAttribute('id') || tableCell.parentNode.getAttribute('id') !== 'colors') {
+                    if (tableCell.classList.contains(selectedColor)) {
+                        tableCell.className='';
+                    }
+                    else {
+                        tableCell.className='';
+                        tableCell.classList.add(selectedColor);
+                    }
+                    const rowIndex = tableCell.parentNode.rowIndex;
+                    const colIndex = tableCell.cellIndex;
+                }
+            });
+        });
+</script>
 
 <div>
     <button class="print" id="print">
